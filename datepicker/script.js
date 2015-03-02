ebca8bdc0be7b06a00df67c26089ca03f41001bc
Beth Stranz <bethstranz@hotmail.com>
2015-03-01 20:08:35 -0600

    phase 9 file update

--- datepicker/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#arrival").datepicker();
-    $("#departure").datepicker();
-    $("button").click(function() {
-        var arrival = $("#arrival").val();
-        var departure = $("#departure").val();
-    });
-});
